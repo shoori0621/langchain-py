cfd1b8628aa5a94afdc7bb075cfe19eabfd5129c
takuya.t <shoori0621@gmail.com>
2025-03-03 11:28:48 +0900

    setup devcontainer

--- app.py
+++ app.py
@@ -10,23 +10,16 @@ openai_api_key = os.getenv("OPENAI_API_KEY")
 
 client = OpenAI(api_key=openai_api_key)
 
-image_url = "https://raw.githubusercontent.com/yoshidashingo/langchain-book/main/assets/cover.jpg"
-
 try:
     response = client.chat.completions.create(
         model="gpt-4o-mini",
         messages=[
-            {
-                "role": "user",
-                "content": [
-                    {"type": "text", "text": "画像を説明してください。"},
-                    {"type": "image_url", "image_url": {"url": image_url}},
-                ],
-            }
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": "こんにちは！私はジョンと言います！"},
         ],
         timeout=10,
     )
 
-    print(response.choices[0].message.content)
-except requests.exceptions.Timeout:
+    print(response.to_json(indent=2))
+except response.exceptions.Timeout:
     print("OpenAI API request timed out")
